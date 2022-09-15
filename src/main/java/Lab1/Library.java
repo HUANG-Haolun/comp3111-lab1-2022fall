dedd1d2a4a89c126887dc9e1ff8b7cb3c5a0d5dc
Mihawk <hhuangbl@connect.ust.hk>
2022-09-15 18:24:05 +0800

    make a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -5,7 +5,10 @@ package Lab1;
 
 public class Library {
 	public static void main(String arg[]) {
-		System.out.println("Hellow, World");
+		Library lib = new Library();
+		if(lib.someLibraryMethod()) {
+			System.out.println("Hellow, World!");
+		}
 	}
     public boolean someLibraryMethod() {
         return true;
